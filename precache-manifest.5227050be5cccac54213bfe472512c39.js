72531b7186512c00a6f3ba00e197323fa3e5f6f4
Natan Carvalho <natanbcpc@gmail.com>
2019-03-24 12:40:16 -0300

    Updates

---
+++ precache-manifest.5227050be5cccac54213bfe472512c39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed76b8aeea7360ded37",
+    "url": "/porta-dos-desesperados/static/js/runtime~main.5ac904d6.js"
+  },
+  {
+    "revision": "c9a7d82efedb3fe5bdd6",
+    "url": "/porta-dos-desesperados/static/js/main.11abacf8.chunk.js"
+  },
+  {
+    "revision": "4677087ad73281f583e8",
+    "url": "/porta-dos-desesperados/static/js/2.129ef102.chunk.js"
+  },
+  {
+    "revision": "c9a7d82efedb3fe5bdd6",
+    "url": "/porta-dos-desesperados/static/css/main.903b1659.chunk.css"
+  },
+  {
+    "revision": "498579d0caebba321fe7ec81f1890b8b",
+    "url": "/porta-dos-desesperados/index.html"
+  }
+];
