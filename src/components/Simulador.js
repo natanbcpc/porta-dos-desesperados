5e7cc97f97a581360a3551deb251fffa4a44d985
Natan Carvalho <natanbcpc@gmail.com>
2019-03-24 14:03:02 -0300

    Optimizing simulation

--- src/components/Simulador.js
+++ src/components/Simulador.js
@@ -16,31 +16,22 @@ class Simulador extends Component {
 	}
 
 	simular() {
-		let simulacoesSempreTroca = [], simulacoesNuncaTroca = [], resultados = [];
-		let reducer = (valorAcumulado, valorAtual) => valorAtual.ganhou ? ++valorAcumulado : valorAcumulado;
+		let resultados = [];
+		let vitoriasTrocando = 0, vitoriasNaoTrocando = 0;
+		let portas = [ true, false, false ];
 		for (let i = 0; i < this.state.qtdSimulacoes; i++) {
-			let portas = [
-				{ premiada : true, aberta : false },
-				{ premiada : false, aberta : false },
-				{ premiada : false, aberta : false }
-			];
-
 			let portaEscolhida = Math.floor(Math.random() * 3);
 
-			if (!portas[0].premiada && portaEscolhida !== 0) {
-				portas[0].aberta = true;
-			} else if (!portas[1].premiada && portaEscolhida !== 1) {
-				portas[1].aberta = true;
-			} else if (!portas[2].premiada && portaEscolhida !== 2) {
-				portas[2].aberta = true;
+			if(portas[portaEscolhida]) {
+				vitoriasNaoTrocando++;
+			} else {
+				vitoriasTrocando++;
 			}
 
-			simulacoesSempreTroca.push({ ganhou : !portas[portaEscolhida].premiada });
-			simulacoesNuncaTroca.push({ ganhou : portas[portaEscolhida].premiada });
 			resultados.push({
 				tentativa : i + 1,
-				vitoriasTrocando : simulacoesSempreTroca.reduce(reducer, 0),
-				vitoriasNaoTrocando : simulacoesNuncaTroca.reduce(reducer, 0)
+				vitoriasTrocando,
+				vitoriasNaoTrocando
 			});
 		}
 
